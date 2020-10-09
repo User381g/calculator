491daed033c66e8b05d5809df5a2913985c12144
User381g <72346967+User381g@users.noreply.github.com>
2020-10-09 21:43:03 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -34,4 +34,19 @@ public class Main {
         }
         return res;
     }
+    
+    public static double squareRoot(int num)   
+    {  
+    
+        double t;  
+        double sqrtroot=num/2;  
+        do   
+        {  
+            t=sqrtroot;  
+            sqrtroot=(t+(num/t))/2;  
+        }   
+        while((t-sqrtroot)!= 0);  
+        return sqrtroot;  
+        }  
+    }
 }
